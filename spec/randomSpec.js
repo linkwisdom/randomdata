cd9c558f3b154295efdc41ba91b471cfdd655c2b
linkwisdom <linkwisdom@qq.com>
2014-04-19 18:29:23 +0800

    修正实现细节

--- spec/randomSpec.js
+++ spec/randomSpec.js
@@ -6,13 +6,80 @@
  */
 var random = require('../src/index');
 
+/**
+ * isBetween 验证数据是否在指定范围
+ * 
+ * @param {number} num 数据
+ * @param {Array} region 范围数组
+ * @return {boolean} 是否在指定范围
+ */
+function isBetween(num, region) {
+    return num >= region[0] && num <= region[1];
+}
+
 describe('jasmine-node', function () {
 
     it('test get random number', function () {
-        expect(random.number(12, 15)).not.toBeGreaterThan(15);
-        expect(random.number(12, 15)).not.toBeLessThan(12);
-        expect(random.floatNumber(12, 15, 0.01)).not.toBeGreaterThan(15);
-        expect(random.floatNumber(12, 15, 0.01)).toBeLessThan(16);
+
+        // 测试产生随机数范围是否正确
+        expect(
+            isBetween(random.number(12, 15), [12, 15])
+        ).toBe( true );
+
+        // 测试负数范围是否正常
+        expect(
+            isBetween(random.number(-100, 500), [-100, 500]) 
+        ).toBe( true );
+
+        // 参数顺序自动反转
+        expect( 
+            isBetween(random.number(100, -100), [-100, 100]) 
+        ).toBe( true );
+
+        // 测试单个参数 (以0作为其它边界)
+        expect(
+            isBetween(random.number(12), [0, 12])
+        ).toBe( true );
+
+        expect(
+            isBetween(random.number(-100), [-100, 0])
+        ).toBe(true);
+
+        expect(random.number(0)).toBe(0);
+
+        // 测试无参数指定： 随机产生正数
+        expect(random.number()).toBeGreaterThan(0);
+    });
+
+    it('test get random number Array', function () {
+        expect(random.number({size: 15}).length).toBe(15);
+
+        // 指定精度，则产生的是固定精度的浮点数
+        var str = random.number(15, 99, {size: 15, precise: 2}).join(',');
+        expect(str.length).toBeLessThan(90);
+    });
+
+    it('test get random floatNumber', function () {
+
+        // 测试结果范围
+        expect( 
+            isBetween( random.floatNumber(12, 15, 2), [12, 15] )
+            ).toBe(true);
+
+        // 测试结果范围
+        expect(
+            isBetween( random.floatNumber(-12, 15, 2), [-12, 15] )
+        ).toBe(true);
+
+        // 指定精度
+        var str = random.floatNumber(20, 99, {size: 15, precise: 2}).join(',');
+        expect(str.length).toBeLessThan(90);
+    });
+
+    it('test get random boolean', function () {
+        var rst = random.bool();
+        rst = rst === true ||  rst === false;
+        expect(rst).toBe(true);
     });
 
     it('test get random strings', function () {
@@ -27,7 +94,9 @@ describe('jasmine-node', function () {
         expect(random.chars(12).length).toBe(12);
 
         // 从默认数组中获取长度为12 - 15的中文字符串
-        expect(random.words(12, 15).length).not.toBeGreaterThan(15);
+        expect(
+            isBetween(random.words(12, 15).length, [12, 15])
+        ).toBe(true);
 
         // 单独数组参数，表示从中取值一个单词
         expect(random.words(source).length).toBe(2);
@@ -35,7 +104,10 @@ describe('jasmine-node', function () {
         // 从源中产生长度为15的随机字符串
         expect(random.words(15, source).length).toBe(15);
 
+        // 从数组中选取一个元素
+        expect(random.getFrom(source).length).toBe(1);
+
         // 从数组中选取单词2词
-        expect(random.getFrom(source, 2).length).toBe(2);
+        expect(random.getFrom(source, 5).length).toBe(5);
     });
 });
--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@
  * @type {number}
  */
 var ONE_DAY = 86400000;
-var MAX_NUM = 1609372800000;
+var MAX_NUM = Math.pow(2, 32);
 
 /**
  * 随机中文字符串，建议用户自己指定随机数组
@@ -33,27 +33,114 @@ var CHARS = 'abcdefg-hijklmnopqrstu-vwxyz-'
     + 'ABCDEFGHIG-KLMNOPQRSTU-VWXYZ-0123456789';
 
 /**
+ * 转化参数
+ *
+ * @inner
+ * @param {number=} min 最小值
+ * @param {number=} max 最大值
+ * @return {Obect}   参数对象
+ */
+function parseArgs(min, max) {
+    var minType = typeof min;
+
+    if (typeof max !== 'number') {
+        max = (minType === 'number') ? 0 : MAX_NUM;
+    }
+
+    min = (minType === 'number') ? min : 0;
+
+    if (max < min) {
+        var t = min;
+        min = max;
+        max = t;
+    }
+
+    return {
+        min: min,
+        max: max
+    };
+}
+
+/**
  * randInt 随机整数
- * 
- * @param  {number} min 最小取值
- * @param  {number} max 最大取值
+ *
+ * @inner
+ * @param {number=} min 最小取值
+ * @param {number=} max 最大取值
  * @return {number}     返回整数
- * @private
  */
 function randInt(min, max) {
-    min || (min = 0);
-    (max === undefined) && (max = MAX_NUM);
-    var num = Math.floor(min + Math.random() * (max - min + 1));
-    return num;
+    var args = parseArgs(min, max);
+    return Math.round((args.max - args.min) * Math.random() + args.min);
+}
+
+/**
+ * randInt 随机浮点数
+ *
+ * @inner
+ * @param {number=} min 最小取值
+ * @param {number=} max 最大取值
+ * @param {number=} precise 精度(0-20)
+ * @return {number}  返回浮点数
+ */
+function randFloat(min, max, precise) {
+    var args = parseArgs(min, max);
+    var num = (args.max - args.min) * Math.random() + args.min;
+    return precise ? +(num).toFixed(precise) : num;
+}
+
+/**
+ * randInt 随机布尔值
+ *
+ * @inner
+ * @return {number} 返回布尔值
+ */
+function randBool() {
+    return Math.random() > 0.5;
+}
+
+/**
+ * initArray 随机整数数组
+ *
+ * @inner
+ * @param {number=} min 最小取值
+ * @param {number=} max 最大取值
+ * @param {number=} size 数组长度
+ * @return {Array.<number>}     随机数组
+ */
+function intArray(min, max, size) {
+    var arr = [];
+    for (var i = size - 1; i >= 0; i--) {
+        arr[i] = randInt(min, max);
+    }
+    return arr;
+}
+
+/**
+ * floatArray 随机浮点数组
+ *
+ * @inner
+ * @param {number=} min 最小取值
+ * @param {number=} max 最大取值
+ * @param {number=} size 数组长度
+ * @param {number=} precise 精度位数(0-20)
+ * @return {Array.<number>}   随机数组
+ */
+function floatArray(min, max, size, precise) {
+    var arr = [];
+    for (var i = size - 1; i >= 0; i--) {
+        arr[i] = randFloat(min, max, precise);
+    }
+    return arr;
 }
 
 /**
  * getFrom 从数组中随机选择num个元素
- * 
- * @param  {Arrary} source 数据源
- * @param  {number} num 个数
- * @return {Array}  返回数组
+ *
  * @public
+ * @param {Array.<*>=} source 数据源
+ * @param {number=} num 个数
+ * @return {Array.<*>}  返回数组
  */
 exports.getFrom = function (source, num) {
     source || (source = CN_WORDS);
@@ -69,57 +156,84 @@ exports.getFrom = function (source, num) {
 
 /**
  * 获取随机整数
- * 
- * @param  {number} min 最小取值
- * @param  {number} max 最大取值
- * @return {number} 随机数值
+ *
  * @public
+ * @param {number=} min 最小取值
+ * @param {number=} max 最大取值
+ * @param {number=} option.size
+ * @param {number=} option.precise
+ * @return {number} 随机数值
  */
-exports.number = function (min, max) {
+exports.number = function (min, max, option) {
+    option = option || arguments[arguments.length - 1] || {};
+
+    if (option.precise > 0) {
+        return exports.floatNumber(min, max, option);
+    }
+
+    if (option.size > 0) {
+        return intArray(min, max, option.size);
+    }
+
+    if (option.precise > 0) {
+        return randFloat(min, max);
+    }
+
     return randInt(min, max);
 };
 
 /**
  * 获取随机浮点数
- * 
- * @param  {number} min 最小取值
- * @param  {number} max 最大取值
- * @param  {number} precise 数值精度0-20
- * @return {number} 随机数值
+ *
  * @public
+ * @param {number=} min 最小取值
+ * @param {number=} max 最大取值
+ * @param {number=} precise 数值精度0-20
+ * @param {number=} option.size
+ * @param {number=} option.precise
+ * @return {number} 随机数值
  */
-exports.floatNumber = function (min, max, precise) {
-    min || (min = 0);
-    (max === undefined) && (max = MAX_NUM);
-    var rst = Math.random() * (max - min) + min;
-    if (precise !== undefined) {
-        rst = +(rst).toFixed(precise);
+exports.floatNumber = function (min, max, option) {
+    option = option || arguments[arguments.length - 1] || {};
+    if (option.size > 0) {
+        return floatArray(min, max, option.size, option.precise);
     }
-    return rst;
+    
+    return randFloat(min, max, option.precise);
+};
+
+/**
+ * 获取随机布尔值
+ *
+ * @public
+ * @return {boolean} 随机布尔值
+ */
+exports.bool = function () {
+    return randBool();
 };
 
 /**
  * 获取随机时间戳
- * @param  {number} pre   当前时间之前多少天
- * @param  {number} after 当前时间之后多少天
- * @return {number} 时间戳值
+ *
  * @public
+ * @param {number=} pre   当前时间之前多少天
+ * @param {number=} after 当前时间之后多少天
+ * @return {number} 时间戳值
  */
 exports.timestamp = function (pre, after) {
     var num = randInt(pre || 0, after || 0);
-
     var timestamp = +(new Date()) + ONE_DAY * num;
-
     return (timestamp).toString(10);
 };
 
 /**
  * 获得随机格式化时间
- * @param  {number} pre   当前时间之前多少天
- * @param  {number} after 当前时间之后多少天
- * @param  {string} format 时间格式
- * @return {number} 格式化时间
+ *
  * @public
+ * @param {number=} pre   当前时间之前多少天
+ * @param {number=} after 当前时间之后多少天
+ * @param {string=} format 时间格式
+ * @return {number} 格式化时间
  */
 exports.formatDate = function (pre, after, format) {
     var moment = require('moment');
@@ -131,23 +245,23 @@ exports.formatDate = function (pre, after, format) {
 /**
  * 获得随机字符串
  * - 长度按utf8长度计算，单个中英文字符长度均为1，
- * 
- * @param  {?number} min 字符最小长度
- * @param  {?number} max 字符最大长度
- * @param  {?Arrary.<string>} source 字符最小长度
- * @return {string} 随机字符串
+ *
  * @public
+ * @param {number=} min 字符最小长度
+ * @param {number=} max 字符最大长度
+ * @param {Array.<string>=} source 字符最小长度
+ * @return {string} 随机字符串
  */
 exports.words = function (min, max, source) {
     var arg1 = arguments[0];
 
     // 如果第一个参数是数组，则视为source
-    if (min instanceof Array) {
+    if (Array.isArray(min)) {
         source = min;
         min = max = 1;
 
     // 否则如果第二个参数为数组，则视为source
-    } else if (max instanceof Array) {
+    } else if (Array.isArray(max)) {
         source = max;
         max = min;
 
@@ -167,11 +281,17 @@ exports.words = function (min, max, source) {
         count += source[idx].length;
 
         if (count <= num) {
+
+            // 如果更改装下当前对象，直接装入
             rst = rst.concat(source[idx]);
-        } else if (arg1 instanceof Array) {
+        } else if (Array.isArray(arg1)) {
+
+            // 如果第一个参数是数组；从数组中随机取一个数
             rst = rst.concat(source[idx]);
             break;
         } else {
+
+            // 如果不能装入；装入部分
             rst = rst.concat(
                 source[idx].slice(0, num - count + source[idx].length)
             );
@@ -185,11 +305,11 @@ exports.words = function (min, max, source) {
 
 /**
  * 随机英文字符
- * 
- * @param  {number} min 最小长度取值
- * @param  {number} max 最大长度取值
- * @return {string} 返回英文字符
+ *
  * @public
+ * @param {number=} min 最小长度取值
+ * @param {number=} max 最大长度取值
+ * @return {string} 返回英文字符
  */
 exports.chars = function (min, max) {
     return this.words(min, max, CHARS);
